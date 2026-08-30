e054732a744079ccce297b35e17c00f1013075ae
kensuke5704 <kensuke5704@gmail.com>
2026-08-30 21:09:00 +0900

    research: rerun fixed60 structural stress

--- scripts/fixed60-structural-stress.ts
+++ scripts/fixed60-structural-stress.ts
@@ -1,3 +1,4 @@
+// research rerun trigger: fixed structural stress, no parameter change
 import fs from "node:fs/promises";
 import path from "node:path";
 import { runBacktest } from "../src/lib/backtest";
